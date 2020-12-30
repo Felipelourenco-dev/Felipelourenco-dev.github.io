64a5485e352be736797a1db75e656d4f2c4f46a2
Felipe Lourenço <72045256+Felipelourenco-dev@users.noreply.github.com>
2020-12-30 16:12:31 -0300

    Add files via upload

---
+++ carrossel.js
@@ -0,0 +1,15 @@
+$(function() {
+
+$(".destaque__produto").slick({
+
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+    prevArrow: $(".seta__voltar"),
+    nextArrow: $(".seta__proximo"),
+
+});
+
+})
